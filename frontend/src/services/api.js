804ffd5aed4237c5459da3aa438dd9a7b312b197
kushagra0333 <kushagrapandey0333@gmail.com>
2026-02-08 13:00:53 +0530

    correct code for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://127.0.0.1:8000/api';
+const API_URL = '/api';
 
 const api = axios.create({
   baseURL: API_URL,
