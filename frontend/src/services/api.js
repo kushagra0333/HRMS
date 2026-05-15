8ff598120f784f727d358e2361cb4a43f7345d76
kushagra0333 <kushagrapandey0333@gmail.com>
2026-05-15 22:36:34 +0530

    fix: Private Network Access prompt and build frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:8000/api'
+  ? `http://${window.location.hostname}:8000/api`
   : '/api';
 
 const api = axios.create({
